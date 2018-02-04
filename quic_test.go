768718f5cf16231aa3956da3138a2d89a851024a
Louis Thibault <l.thibault@sentimens.com>
2018-02-04 11:44:18 +0000

    Add TestNewDialer

--- quic_test.go
+++ quic_test.go
@@ -3,67 +3,73 @@ package quic
 import (
 	"testing"
 
-	"github.com/go-mangos/mangos/protocol/pair"
-	"github.com/go-mangos/mangos/protocol/star"
+	"github.com/go-mangos/mangos"
 )
 
-func TestListen(t *testing.T) {
-	sock, err := star.NewSocket()
+func TestNewDialer(t *testing.T) {
+	var sock mangos.Socket
+
+	addr := "quic://127.0.0.1:9001/clean//up/"
+	trans := NewTransport()
+
+	p, err := trans.NewDialer(addr, sock)
 	if err != nil {
-		t.Errorf("socket create: %s", err)
+		t.Error(err)
+	}
+	d := p.(*dialer)
+
+	if d.Path != "/clean/up" {
+		t.Errorf("expected /clean/up, got %s", d.Path)
 	}
 
-	sock.AddTransport(NewTransport())
-
-	if err = sock.Listen("quic://127.0.0.1:9001/test"); err != nil {
-		t.Errorf("socket listen: %s", err)
+	if d.opt == nil {
+		t.Error("opt is nil")
+	} else if d.sock != sock {
+		t.Error("sock parameter points to unexpected location")
+	} else if d.muxDialer == nil {
+		t.Error("muxDialer is nil")
 	}
 }
 
-func TestDial(t *testing.T) {
-	sock, err := star.NewSocket()
-	if err != nil {
-		t.Errorf("socket create: %s", err)
-	}
+func TestNewListener(t *testing.T) {
 
-	sock.AddTransport(NewTransport())
-
-	if err = sock.Dial("quic://127.0.0.1:9001/test"); err != nil {
-		t.Errorf("socket dial: %s", err)
-	}
 }
 
-func TestIntegration(t *testing.T) {
-	s0, err := pair.NewSocket()
-	if err != nil {
-		t.Errorf("bind sock create: %s", err)
-	}
+func TestMultiplexer(t *testing.T) {
 
-	s1, err := pair.NewSocket()
-	if err != nil {
-		t.Errorf("conn sock create: %s", err)
-	}
-
-	s0.AddTransport(NewTransport())
-	s1.AddTransport(NewTransport())
-
-	if err = s0.Listen("quic://localhost:9090/"); err != nil {
-		t.Errorf("s0 listen: %s", err)
-	}
-
-	if err = s1.Dial("quic://localhost:9090/"); err != nil {
-		t.Errorf("s1 dial: %s", err)
-	}
-
-	t.Log(" SENDING ...")
-	if err = s0.Send([]byte("OH HAI!")); err != nil {
-		t.Errorf("send: %s", err)
-	}
-
-	t.Log(" RECVING ...")
-	if b, err := s1.Recv(); err != nil {
-		t.Errorf("recv: %s", err)
-	} else {
-		t.Log("[ RECV ] ", string(b))
-	}
 }
+
+// func TestIntegration(t *testing.T) {
+// 	s0, err := pair.NewSocket()
+// 	if err != nil {
+// 		t.Errorf("bind sock create: %s", err)
+// 	}
+
+// 	s1, err := pair.NewSocket()
+// 	if err != nil {
+// 		t.Errorf("conn sock create: %s", err)
+// 	}
+
+// 	s0.AddTransport(NewTransport())
+// 	s1.AddTransport(NewTransport())
+
+// 	if err = s0.Listen("quic://localhost:9090/"); err != nil {
+// 		t.Errorf("s0 listen: %s", err)
+// 	}
+
+// 	if err = s1.Dial("quic://localhost:9090/"); err != nil {
+// 		t.Errorf("s1 dial: %s", err)
+// 	}
+
+// 	t.Log(" SENDING ...")
+// 	if err = s0.Send([]byte("OH HAI!")); err != nil {
+// 		t.Errorf("send: %s", err)
+// 	}
+
+// 	t.Log(" RECVING ...")
+// 	if b, err := s1.Recv(); err != nil {
+// 		t.Errorf("recv: %s", err)
+// 	} else {
+// 		t.Log("[ RECV ] ", string(b))
+// 	}
+// }
