a10c178f4b8f9f08cf0bfa07778f64b9f25a407b
Louis Thibault <l.thibault@sentimens.com>
2018-02-04 11:55:15 +0000

    Add tests

--- quic_test.go
+++ quic_test.go
@@ -6,33 +6,87 @@ import (
 	"github.com/go-mangos/mangos"
 )
 
-func TestNewDialer(t *testing.T) {
-	var sock mangos.Socket
-
-	addr := "quic://127.0.0.1:9001/clean//up/"
-	trans := NewTransport()
-
-	p, err := trans.NewDialer(addr, sock)
-	if err != nil {
-		t.Error(err)
-	}
-	d := p.(*dialer)
-
-	if d.Path != "/clean/up" {
-		t.Errorf("expected /clean/up, got %s", d.Path)
-	}
-
-	if d.opt == nil {
+func TestNewTransport(t *testing.T) {
+	trans := NewTransport().(*transport)
+	if trans.opt == nil {
 		t.Error("opt is nil")
-	} else if d.sock != sock {
-		t.Error("sock parameter points to unexpected location")
-	} else if d.muxDialer == nil {
-		t.Error("muxDialer is nil")
+	} else if trans.routes == nil {
+		t.Error("router is nil")
+	} else if trans.listeners == nil {
+		t.Error("listeners is nil")
+	} else if trans.sessions == nil {
+		t.Error("sessions is nil")
+	} else if trans.Scheme() != "quic" {
+		t.Errorf("expected sheme to be `quic `, got %s", trans.Scheme())
 	}
 }
 
-func TestNewListener(t *testing.T) {
+func TestNewDialer(t *testing.T) {
+	var sock mangos.Socket
+	trans := NewTransport()
 
+	t.Run("SuccessfulInit", func(t *testing.T) {
+		addr := "quic://127.0.0.1:9001/clean//up/"
+
+		p, err := trans.NewDialer(addr, sock)
+		if err != nil {
+			t.Error(err)
+		}
+		d := p.(*dialer)
+
+		if d.Path != "/clean/up" {
+			t.Errorf("expected /clean/up, got %s", d.Path)
+		}
+
+		if d.opt == nil {
+			t.Error("opt is nil")
+		} else if d.sock != sock {
+			t.Error("sock parameter points to unexpected location")
+		} else if d.muxDialer == nil {
+			t.Error("muxDialer is nil")
+		}
+	})
+
+	t.Run("BadURL", func(t *testing.T) {
+		addr := "xxx"
+		if _, err := trans.NewDialer(addr, sock); err == nil {
+			t.Error("should have failed due to invalid URL")
+		}
+	})
+}
+
+func TestNewListener(t *testing.T) {
+	var sock mangos.Socket
+	trans := NewTransport()
+
+	t.Run("SuccessfulInit", func(t *testing.T) {
+		addr := "quic://127.0.0.1:9001/clean//up/"
+
+		p, err := trans.NewListener(addr, sock)
+		if err != nil {
+			t.Error(err)
+		}
+		l := p.(*listener)
+
+		if l.Path != "/clean/up" {
+			t.Errorf("expected /clean/up, got %s", l.Path)
+		}
+
+		if l.opt == nil {
+			t.Error("opt is nil")
+		} else if l.sock != sock {
+			t.Error("sock parameter points to unexpected location")
+		} else if l.muxListener == nil {
+			t.Error("muxListener is nil")
+		}
+	})
+
+	t.Run("BadURL", func(t *testing.T) {
+		addr := "xxx"
+		if _, err := trans.NewListener(addr, sock); err == nil {
+			t.Error("should have failed due to invalid URL")
+		}
+	})
 }
 
 func TestMultiplexer(t *testing.T) {
